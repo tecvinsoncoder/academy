e2eb8614a240dd6e3c78f1a3dc71f2e91ef0045d
Oladejo Adesola <adesola@tecvinson.com>
2023-12-17 14:50:29 +0000

    Entity Migrations and Initial Data Update

--- Academy.Core/Entities/Department.cs
+++ Academy.Core/Entities/Department.cs
@@ -5,6 +5,7 @@ namespace Academy.Core.Entities
         public string Name {set; get;} = string.Empty;
         public string Code {set; get;} = string.Empty;
         public Guid FacultyId {set;get;}
+        public Faculty? Faculty {set;get;}
         public ICollection<Course>? Courses {set; get;}
     }
 }
--- Academy.Presentation/Program.cs
+++ Academy.Presentation/Program.cs
@@ -1,6 +1,11 @@
+using Academy.Repositories.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AcademyContext>( options => 
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
